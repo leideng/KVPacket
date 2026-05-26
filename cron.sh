07184849ab5425529698c5f5e56c20298c956b6b
ldeng <ldeng.sjtu@gmail.com>
2026-05-27 00:05:28 +0800

    add tonight training task

---
+++ cron.sh
@@ -0,0 +1,8 @@
+#0526
+
+python run_train_filler.py packet_wrapper_config/qwen_3_8b/hotpot_qa/8_8.json &> dev/training.0526.qwen3-8b.hotpotqa.txt
+python run_train_filler.py packet_wrapper_config/qwen_3_8b/musique/8_8.json &> dev/training.0526.qwen3-8b.musique.txt
+python run_train_filler.py packet_wrapper_config/qwen_3_8b/niah/8_8.json &> dev/training.0526.qwen3-8b.niah.txt
+
+
+
